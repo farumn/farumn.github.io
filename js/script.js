41499688f0c8ed8f9f2d681f225d297e4a810a93
Faruman <52614879+farumn@users.noreply.github.com>
2021-12-03 02:42:46 +0800

    Fix Bug

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-$(function(){
-    if ($(window).width() >= 992){
-        $('body').attr("data-bs-offset","90"); 
+if ($(window).width() >= 992){
+    $('body').attr("data-bs-offset","90"); 
+}
+else {
+    $('body').attr("data-bs-offset","260"); 
+}
+$('.scrollPage').click(function(e){
+    e.preventDefault();
+    var section = $(this).attr("href");
+    var windowWidth = $(window).width();
+    if(windowWidth >= 992){
+        $('html,body').animate({
+            scrollTop: $(section).offset().top - 70
+        }, "easeInOutExpo");
     }
     else {
-        $('body').attr("data-bs-offset","260"); 
+        $('html,body').animate({
+            scrollTop: $(section).offset().top - 240
+        }, "easeInOutExpo");  
     }
-    $('.scrollPage').click(function(e){
-        e.preventDefault();
-        var section = $(this).attr("href");
-        var windowWidth = $(window).width();
-        if(windowWidth >= 992){
-            $('html,body').animate({
-                scrollTop: $(section).offset().top - 70
-            }, "easeInOutExpo");
-        }
-        else {
-            $('html,body').animate({
-                scrollTop: $(section).offset().top - 240
-            }, "easeInOutExpo");  
-        }
-    });
 });
